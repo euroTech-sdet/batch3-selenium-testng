d3666b8ee872a1e6cd3b56d24f4e2a4b3042de2d
Erhan <erhanilgar@gmail.com>
2022-04-07 21:21:19 +0100

    Day15 testNG

--- src/test/java/com/eurotech/day15TypeOfElements/AttributeTest.java
+++ src/test/java/com/eurotech/day15TypeOfElements/AttributeTest.java
@@ -1,6 +1,8 @@
 package com.eurotech.day15TypeOfElements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
@@ -12,6 +14,15 @@ public class AttributeTest {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.get("https://demo.aspnetawesome.com/");
+
+        WebElement radioBtn = driver.findElement(By.xpath("(//input[@type='radio'])[10]"));
+
+        System.out.println(radioBtn.getAttribute("type"));
+        System.out.println(radioBtn.getAttribute("value"));
+        System.out.println(radioBtn.getAttribute("name"));
+
+        //not: bazen getText ile alamadigimiz text leri getAttribute methodu ile aliyoruz.
+
 
     }
 }
