c0002c37258f35fc825d6e48c9fcfd414dbe77cd
Erhan <erhanilgar@gmail.com>
2022-04-25 10:56:08 +0100

    Day16 alert

--- src/test/java/com/eurotech/day16TypeOfElements2/PopUpAndAlerts.java
+++ src/test/java/com/eurotech/day16TypeOfElements2/PopUpAndAlerts.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
@@ -25,6 +26,12 @@ public class PopUpAndAlerts {
          driver.findElement(By.xpath("(//*[text()='Confirm'])[1]")).click();
          driver.findElement(By.xpath("//span[text()='Yes']")).click();
 
+    }
+
+    //@Test
+    public void test3() {
+
+        System.out.println("Gerek yok");
     }
 
     @Test
@@ -52,11 +59,6 @@ public class PopUpAndAlerts {
         //her bir js alert ti tikla ve asagida beliren yaziyin textini alip verify et.
         //expectedText = You successfully clicked an alert
         //actualText =
-
-
-
-
-
 
 
     }
