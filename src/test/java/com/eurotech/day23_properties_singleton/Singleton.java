b29011298319fa809f54b16c152769c8ee722622
Hasan Oztas <hasan.oztas@finspiretech.com>
2022-05-05 22:57:48 +0300

    day23_confProperties, ConfReader, Driver added, tests implemented

---
+++ src/test/java/com/eurotech/day23_properties_singleton/Singleton.java
@@ -0,0 +1,26 @@
+package com.eurotech.day23_properties_singleton;
+
+public class Singleton {
+
+    // Singleton class will have private constructor
+    // So other classes cannot create object of this class
+
+    private Singleton() {};
+
+    private static String str;
+
+    public static String getInstance() {
+
+        // if str has no value, initialize it and then return
+        if(str == null) {
+            System.out.println("str is null, assigning a value to it");
+            str = "abcdef";
+        }else {
+
+            // if it has value, just return it
+            System.out.println("str has a value, just returning it");
+        }
+        return str;
+    }
+
+}
